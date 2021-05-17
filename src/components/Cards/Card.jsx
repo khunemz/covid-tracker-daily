c509657f87ded33603e69c9623d3f30477d14510
khunemz <khunemz@gmail.com>
2021-05-17 21:18:16 +0700

    restructure

---
+++ src/components/Cards/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div>
+      <h2>Card</h2>
+    </div>
+  )
+}
+
+export default Card
---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CountryPicker() {
+  return (
+    <div>
+      <h2>Country picker</h2>
+    </div>
+  )
+}
+
+export default CountryPicker
